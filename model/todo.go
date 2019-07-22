72cfab2d62d42ec72329e4d9a09dbcab7526261c
= <=>
2019-07-22 16:38:32 +0300

    Added models

--- model/todo.go
+++ model/todo.go
@@ -1,1 +1,12 @@
 package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Todo struct {
+	gorm.Model
+	Title string `json:"title"`
+	Description string `json:"description"`
+	UserId uint `json:"user_id"`
+}
--- model/user.go
+++ model/user.go
@@ -1,1 +1,19 @@
 package model
+
+import(
+	"github.com/jinzhu/gorm"
+	"github.com/dgrijalva/jwt-go"
+	)
+
+type Token struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+type User struct {
+	gorm.Model
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string
+	Token string `json:"token"; sql:"-"`
+}
